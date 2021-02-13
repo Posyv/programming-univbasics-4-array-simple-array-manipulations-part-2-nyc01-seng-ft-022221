c5656efac4d6cce529f7aad3ad766934c776421e
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 21:50:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,6 +15,8 @@ def using_flatten (instruments)
   instruments.flatten
 end
 
-def using_delete (no_offense_steven)
+def using_delete (no_offense_steven, Steven)
   no_offense_steven.delete("Steven")
 end
+
+def using_delete_at
