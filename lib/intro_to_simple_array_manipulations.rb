f846b42602678570d9d5a6eaec690e644f1e9cf4
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 21:36:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,9 @@
+def using_concat (my_favorite_things, more_favs)
+  extrafavs = []
+  extrafavs.concat(my_favorite_things)
+end
+
+
 def using_insert (list_of_programming_languages, another_language)
   list_of_programming_languages.insert(another_language)
 end
