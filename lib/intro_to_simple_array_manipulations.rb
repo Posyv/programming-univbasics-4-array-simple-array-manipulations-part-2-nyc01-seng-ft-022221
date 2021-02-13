3fcc0ea78da219fe1155344006897f7bf8f59ffa
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 21:41:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,7 @@ end
 def using_insert (list_of_programming_languages, another_language)
   list_of_programming_languages.insert(another_language.to_s)
 end
+
+def using_uniq (haircuts)
+  haircuts.uniq
+end
